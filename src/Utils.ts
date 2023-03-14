efddec514817290c5c45173036a9a70933242c76
Devid <d.zanon93@gmail.com>
2023-03-14 15:24:08 +0100

    Fix regex for aria2 check on non-English machines

--- src/Utils.ts
+++ src/Utils.ts
@@ -214,11 +214,12 @@ export function checkRequirements(): void {
     }
 
     try {
-        const versionRegex = new RegExp(/aria2 version (.*)/);
-        const aira2Ver: string = execSync('aria2c --version').toString().split('\n')[0];
+        //const versionRegex = new RegExp(/aria2 version (.*)/);
+        const versionRegex = new RegExp(/aria2 .* (\d+\.\d+\.\d+.*)/);
+        const aria2Ver: string = execSync('aria2c --version').toString().split('\n')[0];
 
-        if (versionRegex.test(aira2Ver)) {
-            logger.verbose(`Using ${aira2Ver}\n`);
+        if (versionRegex.test(aria2Ver)) {
+            logger.verbose(`Using ${aria2Ver}\n`);
         }
         else {
             throw new Error();
